c231740a96ec5739e84ab8b354dce5ec8452b27a
omero <omero@liveperson.com>
2018-06-12 15:20:01 +0300

    add twitter authenciation

--- src/main/java/com/fastfur/messaging/data/Tweet.java
+++ src/main/java/com/fastfur/messaging/data/Tweet.java
@@ -2,17 +2,83 @@ package com.fastfur.messaging.data;
 
 import twitter4j.Status;
 
+import java.util.Date;
+
 public class Tweet implements Identity {
-    public String id;
-    public Status status;
+    private String id;
+    private int favoriteCount;
+    private int retweetCount;
+    private String text;
+    private Date createdAt;
+    private String language;
+    private String source;
 
     public Tweet(String id, Status status){
         this.id = id;
-        this.status = status;
+        this.favoriteCount = status.getFavoriteCount();
+        this.retweetCount =status.getRetweetCount();
+        this.text = status.getText();
+        this.createdAt = status.getCreatedAt();
+        this.language = status.getLang();
+        this.source= status.getSource();
+
+
     }
+
+    public int getFavoriteCount() {
+        return favoriteCount;
+    }
+
+    public void setFavoriteCount(int favoriteCount) {
+        this.favoriteCount = favoriteCount;
+    }
+
+    public int getRetweetCount() {
+        return retweetCount;
+    }
+
+    public void setRetweetCount(int retweetCount) {
+        this.retweetCount = retweetCount;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+
 
 
     public String getKey() {
         return id;
     }
+
+
 }
