1bee1e20e59cde81bf262183281509732a0d6a56
omero <omero@liveperson.com>
2018-06-21 18:29:11 +0300

    join exercise

--- src/main/java/com/fastfur/messaging/streaming/BranchTopology.java
+++ src/main/java/com/fastfur/messaging/streaming/BranchTopology.java
@@ -2,7 +2,7 @@ package com.fastfur.messaging.streaming;
 
 import com.fastfur.messaging.data.Tweet;
 import com.fastfur.messaging.producer.Queries;
-import com.fastfur.messaging.producer.TweetProducer;
+import com.fastfur.messaging.producer.twitter.TweetProducer;
 import com.fastfur.messaging.serde.TweetSerde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.Consumed;
