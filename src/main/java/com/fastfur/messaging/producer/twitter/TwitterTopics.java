82f2c91b653218a53bd4e120dee95b3b8c130bd8
omero <omero@liveperson.com>
2018-06-21 18:29:48 +0300

    join exercise

---
+++ src/main/java/com/fastfur/messaging/producer/twitter/TwitterTopics.java
@@ -0,0 +1,6 @@
+package com.fastfur.messaging.producer.twitter;
+
+public class TwitterTopics {
+    public static final String TWITTERS_TOPIC= "twitters";
+    public static final String GOT_RESPONDED_TOPIC ="got_reponded";
+}
