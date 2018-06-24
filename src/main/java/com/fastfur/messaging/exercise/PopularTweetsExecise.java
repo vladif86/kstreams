b4bf79dbe06cc618d6e234e86a4939cc5ce84501
omero <omero@liveperson.com>
2018-06-24 18:03:14 +0300

    remove derpricted function

--- src/main/java/com/fastfur/messaging/exercise/PopularTweetsExecise.java
+++ src/main/java/com/fastfur/messaging/exercise/PopularTweetsExecise.java
@@ -1,6 +1,7 @@
 package com.fastfur.messaging.exercise;
 
 import com.fastfur.messaging.data.Tweet;
+import com.fastfur.messaging.producer.twitter.TwitterTopics;
 import com.fastfur.messaging.serde.TweetSerde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.Consumed;
@@ -8,12 +9,10 @@ import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
-import org.apache.kafka.streams.kstream.internals.TimeWindow;
 
 import java.util.Properties;
 
-import static com.fastfur.messaging.streaming.DevicesTopology.DEVICE_TOPIC_NAME;
-import static com.fastfur.messaging.streaming.DevicesTopology.INPUT_TOPIC_NAME;
+
 
 public class PopularTweetsExecise {
 
@@ -25,8 +24,7 @@ public class PopularTweetsExecise {
 
 
     public static void main(String[] args) throws Exception {
-//        TweetProducer tp = new TweetProducer();
-//        tp.produceTweets( INPUT_TOPIC_NAME, Queries.getQueries() );
+
 
         Properties config = new Properties();
         config.put( StreamsConfig.APPLICATION_ID_CONFIG, "my-first-tweet-ks1" );
@@ -36,18 +34,11 @@ public class PopularTweetsExecise {
 
 
         StreamsBuilder builder = new StreamsBuilder();
-        KStream<String, Tweet> stream = builder.stream( INPUT_TOPIC_NAME, Consumed.with( Serdes.String(), new TweetSerde() ) );
-        KStream<String, Tweet> deviceStream = builder.stream( DEVICE_TOPIC_NAME, Consumed.with( Serdes.String(), new TweetSerde() ) );
-
-        stream.filter(
-                new Predicate<String, Tweet>() {
-                    @Override
-                    public boolean test(String key, Tweet value) {
-                        return value.getFavoriteCount() == 0;
-                    }
-                });
-
-        KTable<Windowed<String>, Tweet> longSums  =  stream.groupBy( (k,v) -> v.getLanguage())
+        KStream<String, Tweet> stream = builder.stream( TwitterTopics.TWITTERS_TOPIC, Consumed.with( Serdes.String(), new TweetSerde() ) );
+        stream.foreach((k, v) -> System.out.println( "start -> " +k.toString() + "   key -> " + v.toString() )  );
+        KTable<Windowed<String>, Tweet> longSums  =
+                stream.filter((k,v) ->  v.getFavoriteCount() > 10)
+                .groupBy( (k,v) -> v.getLanguage())
                 .windowedBy( TimeWindows.of( 60000L ).until( 60000L*10 ) )
                 .reduce( (v1,v2 ) -> v1.getFavoriteCount() > v2.getFavoriteCount()? v1 : v2 );
         longSums.foreach( (k, v) -> System.out.println( "start -> " + k.window().start() +  "  key -> " + k.key() ) );
