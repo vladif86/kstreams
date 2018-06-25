7b1ba9a0fe3f7e4113db76823da16b9c0e1c3873
omero <omero@liveperson.com>
2018-06-25 10:56:20 +0300

    remove derpricted function

--- src/main/java/com/fastfur/messaging/producer/twitter/TwitterTopics.java
+++ src/main/java/com/fastfur/messaging/producer/twitter/TwitterTopics.java
@@ -3,4 +3,5 @@ package com.fastfur.messaging.producer.twitter;
 public class TwitterTopics {
     public static final String TWITTERS_TOPIC= "twitters";
     public static final String GOT_RESPONDED_TOPIC ="got_reponded";
+    public static final String DEVICES_TOPIC ="device";
 }
